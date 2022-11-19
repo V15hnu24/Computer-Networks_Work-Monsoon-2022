d625ef82a70418d7d46915a4bb25eb1ebb713f03
V15hnu24 <799vishnu530@gmail.com>
2022-11-19 20:42:57 +0530

    Commit of Assignment 3

--- Network simulator Assignment 3/QueuingDelay.py
+++ Network simulator Assignment 3/QueuingDelay.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-file = open("C:\\Users\\799vi\\Desktop\\Courses\\Computer networks\\Network simulator Assignment 3\\Q1\\tcp-example.tr", "r")
+
+file = open("tcp-example.tr", "r")
 
 data = file.readlines()
 
@@ -10,39 +11,44 @@ y = []
 
 Q = {}
 
-delay_t = 0
-packets_t = 0
-temp = 0
+def fun():
+    delay_t = 0
+    packets_t = 0
+    temp = 0
 
-for i in data:
-    j = i.split()
+    for i in data:
+        j = i.split()
 
-    sequence = int(j[36][4:])
-    time = float(j[1])
-    enq = j[0]=="+"
-    deq = j[0]=="-"
+        sequence = int(j[36][4:])
+        time = float(j[1])
+        enq = j[0]=="+"
+        deq = j[0]=="-"
 
-    if enq:
-        Q[sequence] = time
-    elif deq:
-        if sequence in Q:
-            u = time - Q[sequence]
-            delay_t += time - Q[sequence]
-            packets_t += 1
+        if enq:
+            Q[sequence] = time
+        elif deq:
+            if sequence in Q:
+                u = time - Q[sequence]
+                delay_t += time - Q[sequence]
+                packets_t += 1
 
-            if len(x) > 0 and x[-1] == time:
-                y[-1] = (y[-1] * (temp + u)) / (temp + 1)
-                temp += 1
-            else:
-                temp = 1
-                x.append(time)
-                y.append(u)
+                if len(x) > 0 and x[-1] == time:
+                    y[-1] = (y[-1] * (temp + u)) / (temp + 1)
+                    temp += 1
+                else:
+                    temp = 1
+                    x.append(time)
+                    y.append(u)
 
-X = np.array(x)
-Y = np.array(y)
+    X = np.array(x)
+    Y = np.array(y)
 
-plt.plot(X, Y,"g")
-plt.xlabel("Time")
-plt.ylabel("Queuing Delay")
-plt.title("Queuing Delay vs Time")
-plt.show()
+    plt.plot(X, Y,"g")
+    plt.xlabel("Time")
+    plt.ylabel("Queuing Delay")
+    plt.title("Queuing Delay vs Time")
+    plt.show()
+
+
+if __name__ == "__main__":
+    fun()
